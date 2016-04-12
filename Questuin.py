23ca62779825aa098c3b153b15198cd0bb781fea
ambikaprasad <ambika.singh1992@gmail.com>
2016-04-12 22:51:12 +0530

    Create Questuin.py

---
+++ Questuin.py
@@ -0,0 +1,20 @@
+Questype=((1,'MCQ Single Correct'),(2,'MCQ multiple correct'),(3,'Group MCQ single correct'),(4,'True and false'),
+(5,'Integer Type'),(6,'Matrix match'),(7,'Linked questions'),(8,'Assertion and Reasons type'),(9,'Subjective or descriptive type'),
+(10,'Group (Common data) Subjective or descriptive type'))
+
+Question model:
+Questype =
+Question =
+added_date=
+added_by=
+
+option model:
+question=
+option=
+
+QuestionUser:
+question=
+user=
+
+
+
