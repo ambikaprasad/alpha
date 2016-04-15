6092ae1eddc75805e04b0c2ad60a80b268f9aa33
kamta1394 <kamta.singhpython@gmail.com>
2016-04-15 19:03:43 +0530

    admin changes

--- questionmanager/admin.py
+++ questionmanager/admin.py
@@ -3,13 +3,13 @@ from models import *
 from django.conf import settings
 from datetime import datetime
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','description','slug','active','created_date','modified_date','added_by')
-    list_editable = ('active',)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name','description','slug','active','is_category','created_date','modified_date','added_by')
+    list_editable = ('active','slug')
     exclude = ('slug','added_by')
-    search_fields = ('tag_name',)
+    search_fields = ('name',)
     def get_queryset(self, request):
-        qs = super(CategoryAdmin, self).get_queryset(request)
+        qs = super(TagAdmin, self).get_queryset(request)
         if request.user.is_superuser:
             return qs
         return qs.filter(added_by=request.user)
@@ -17,7 +17,21 @@ class CategoryAdmin(admin.ModelAdmin):
         obj.added_by=request.user
         obj.save()
 
+class QuestionManagerAdmin(admin.ModelAdmin):
+    list_display = ('questype','question','parentquestion','category','tag','status','correct_answer','explanation','user','slug','default_marks','random_option','added_date','added_by')
+    search_fields = ('question',)
+    exclude = ('slug','added_by')
+    def save_model(self, request, obj, form, change):
+        obj.added_by=request.user
+        obj.save()
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('question','option','active')
+    search_fields = ('question__question')
+    exclude = ('slug','added_by')
 
 
-admin.site.register(Category,CategoryAdmin)
+
+admin.site.register(Tag,TagAdmin)
+admin.site.register(QuestionManager,QuestionManagerAdmin)
+admin.site.register(Option,OptionAdmin)
 
--- questionmanager/models.py
+++ questionmanager/models.py
@@ -7,52 +7,48 @@ Questype=((1,'MCQ Single Correct'),(2,'MCQ multiple correct'),(3,'Group MCQ sing
 (5,'Integer Type'),(6,'Matrix match'),(7,'Linked questions'),(8,'Assertion and Reasons type'),(9,'Subjective or descriptive type'),
 (10,'Group (Common data) Subjective or descriptive type'))
 
-class Category(models.Model):
-    name= models.CharField(max_length=255)
-    description= models.TextField(null=True,blank=True)
-    slug=models.SlugField(max_length=150,unique=True)
-    active=models.BooleanField(default=False)
-    created_date=models.DateTimeField(auto_now_add=True)
-    modified_date=models.DateTimeField(auto_now=True)
-    added_by=models.ForeignKey(User)
-    def __unicode__(self):
-        return self.tag_name
-    def save(self, *args, **kwargs):
-        if self.slug is None or self.slug == '':
-            self.slug=slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
-
 class Tag(models.Model):
     name= models.CharField(max_length=255)
     description= models.TextField(null=True,blank=True)
     slug=models.SlugField(max_length=150,unique=True)
     active=models.BooleanField(default=False)
+    is_category=models.BooleanField(default=False)
     created_date=models.DateTimeField(auto_now_add=True)
     modified_date=models.DateTimeField(auto_now=True)
     added_by=models.ForeignKey(User)
     def __unicode__(self):
-        return self.tag_name
+        return self.name
     def save(self, *args, **kwargs):
         if self.slug is None or self.slug == '':
             self.slug=slugify(self.name)
         super(Tag, self).save(*args, **kwargs)
 
 class QuestionManager(models.Model):
-  Questype = models.IntegerField(max_length=2,choices=Questype, default=1)
-  Question =  models.TextField()
-  ParentQuestion = models.ForeignKey("self",null=True,blank=True)
-  category = models.ManyToManyField(Category)
-  Tag = models.ManyToManyField(Tag)
-  added_date = models.DateTimeField(auto_now_add=True)
-  added_by = models.ForeignKey(User)
+  questype = models.IntegerField(max_length=2,choices=Questype, default=1)
+  question =  models.TextField(unique=True)
+  parentquestion = models.ForeignKey("self",null=True,blank=True)
+  category = models.ManyToManyField(Tag,limit_choices_to={'is_category': True})
+  tag = models.ManyToManyField(Tag,limit_choices_to={'is_category': False})
   status = models.BooleanField(default=False)
   correct_answer = models.TextField()
   explanation=models.TextField(null=True,blank=True)
-  taguser= models.ManyToManyField(User)
+  user= models.ManyToManyField(User)
+  added_date = models.DateTimeField(auto_now_add=True)
+  added_by = models.ForeignKey(User)
+  default_marks=models.IntegerField(default=1)
+  random_option=models.BooleanField(default=False)
+  slug=models.TextField(unique=True)
+  def __unicode__(self):
+        return self.question
+  def save(self, *args, **kwargs):
+        if self.slug is None or self.slug == '':
+            self.slug=slugify(self.name)
+        super(QuestionManager, self).save(*args, **kwargs)
 
 class Option(models.Model):
   question= models.ForeignKey(QuestionManager)
   option= models.TextField()
+  active=models.BooleanField(default=True)
 
 
 
