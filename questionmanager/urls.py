5ab078a4157393e9034d4539a446f5a1305c0ee8
kamta1394 <kamta.singhpython@gmail.com>
2016-04-16 23:06:18 +0530

    template integration

--- questionmanager/urls.py
+++ questionmanager/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('tutorials.views',
+urlpatterns = [
     # Examples:
     #url(r'^tutorials/$', 'tutorials', name='tutorials'),
     url(r'^$', TemplateView.as_view(template_name='questionmanager/index.html'), name="home"),
     url(r'^category/$', TemplateView.as_view(template_name='questionmanager/categories.html'), name="category"),
-    )
+    ]
 
